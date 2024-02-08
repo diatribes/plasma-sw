1374784f20e4e55dc0c714ae29711fa1e35b98d7
Matt Vianueva <matt.vianueva@gmail.com>
2024-02-08 07:25:31 -0500

    Screenshot updated

--- src/main.c
+++ src/main.c
@@ -101,7 +101,7 @@ int main(int argc, char * argv[])
 
     SetTargetFPS(TARGET_FPS);
     DisableCursor();
-    ToggleBorderlessWindowed();
+    //ToggleBorderlessWindowed();
     while (!WindowShouldClose()) {
 
         time = GetTime();
